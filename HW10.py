4c9f94beb27faa70f50c7fa11acdc7210ee79569
sudashas <suhas.das@emory.edu>
2025-12-05 23:10:53 -0500

    final 3

--- HW10.py
+++ HW10.py
@@ -1,4 +1,5 @@
 import os
+import numpy as np
 import scipy.io as sio
 from self_py_fun.HW10Fun import *
 from sklearn.linear_model import LogisticRegression as LR
@@ -26,7 +27,7 @@ bp_low = 0.5
 bp_upp = 6
 electrode_num = 16
 # Change the following directory to your own one.
-parent_dir = '/Users/tma33/Library/CloudStorage/OneDrive-EmoryUniversity/Emory/Rollins SPH/2025/BIOS-584/python_proj'
+parent_dir = os.getcwd()
 parent_data_dir = '{}/data'.format(parent_dir)
 time_index = np.linspace(0, 800, 25)
 electrode_name_ls = ['F3', 'Fz', 'F4', 'T7', 'C3', 'Cz', 'C4', 'T8', 'CP3', 'CP4', 'P3', 'Pz', 'P4', 'PO7', 'PO8', 'Oz']
